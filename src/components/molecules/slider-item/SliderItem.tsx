6e20bc6762c022761881fbf311a0b16ea2df002c
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-12-29 17:52:38 -0500

    style: add hover to slider section

--- src/components/molecules/slider-item/SliderItem.tsx
+++ src/components/molecules/slider-item/SliderItem.tsx
@@ -2,19 +2,21 @@ import {AgeAdvisor} from "@/components/atoms/age-advisor/AgeAdvisor";
 
 function SliderItem() {
   return (
-    <section className="section-slider">
-      <div className="slider-item">
-        <img className="slider-item__image" src="/slider-icons/imagen3.webp"/>
-      </div>
+    <>
 
-      <div className="title-container">
-        <img className="image-title" src="/slider-icons/imagen3-title.webp"/>
-        <div className="text-container">
-          <span>Todos los episodios ya disponibles</span>
-          <div className="text-category"><AgeAdvisor>16</AgeAdvisor> <span>2024 Acción y aventura, Espionaje</span></div>
+      <div className="slider-item">
+        <img className="slider-item__image" src="/slider-icons/imagen3.webp" alt=""/>
+
+        <div className="title-container">
+          <img className="image-title" src="/slider-icons/imagen3-title.webp"/>
+          <div className="text-container">
+            <span>Todos los episodios ya disponibles</span>
+            <div className="text-category"><AgeAdvisor>16</AgeAdvisor> <span>2024 Acción y aventura, Espionaje</span></div>
+          </div>
         </div>
       </div>
-    </section>
+
+    </>
 
   );
 }
